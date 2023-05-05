cdea42edeef4cffa4bd1b4726cc9f8c92968f715
userDIMd <132671987+userDIMd@users.noreply.github.com>
2023-05-05 15:37:31 +0300

    Update table_printer.py
    tableData = [['apples', 'oranges', 'cherries', 'banana'],
                 ['Alice', 'Bob', 'Carol', 'David'],
                 ['dogs', 'cats', 'moose', 'goose']]
    for column in range(4):
        for row in range(3):
           if row < 2:
              print(tableData[row][column], end = ' ')
           else:
              print(tableData[row][column])

--- 06_Manipulating_Strings/Practice_Projects/table_printer.py
+++ 06_Manipulating_Strings/Practice_Projects/table_printer.py
@@ -1,6 +1,4 @@
 
-
-
 def column_wideth(tabel):
     colWideths = [0] * len(tabel)
     for i in range(len(tabel)):
@@ -8,8 +6,6 @@ def column_wideth(tabel):
             if len(tabel[i][j]) > colWideths[i]:
                 colWideths[i] = len(tabel[i][j])
     return colWideths
-
-
 
 def print_table(table, colWideths):
     for i in range(len(table[0])): # i = 4 meansing the output has 4 rows
